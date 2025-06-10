24ab4d0a29556c86b1fe883c4694898d299ee0fd
Danrleyf <danrleyf@gmail.com>
2025-06-09 23:51:20 -0300

    alterações

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/comanda-junina/',
+  base: '/',
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: resolve(__dirname, 'index.html'),
-      output: {
-        manualChunks: undefined
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src')
-    }
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      target: 'es2020'
-    }
+    sourcemap: true
   }
 })
