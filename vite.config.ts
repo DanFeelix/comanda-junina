6e2a52394700646effde04bf9b8aa7249c44c9d6
Danrleyf <danrleyf@gmail.com>
2025-06-09 23:19:34 -0300

    alterações

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: './',
   plugins: [react()],
   base: '/comanda-junina/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom'],
-          'mui-vendor': ['@mui/material', '@mui/icons-material']
-        }
-      }
-    }
-  },
-  server: {
-    port: 3000,
-    open: true
-  }
 })
